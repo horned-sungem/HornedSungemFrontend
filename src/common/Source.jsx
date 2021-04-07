a58e06600d0e9fca2d4a391d355438da168c74f3
Inluminous <lenmisch@gmail.com>
2021-04-07 19:55:01 +0200

    Add routing

---
+++ src/common/Source.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+export const Source = () => (
+    <Router>
+        <Switch>
+            {/*<Route component={App} path='/' exact/> */}
+        </Switch>
+    </Router>  
+);
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Source } from './common/Source'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Source />
   </React.StrictMode>,
   document.getElementById('root')
 )
