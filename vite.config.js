96d4bfc2c4ae9d99395a66059560c51e01ac1ef4
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2021-04-07 22:30:38 +0200

    more toolchain improvements

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import eslintPlugin from 'vite-plugin-eslint';
-
+import ViteFonts from 'vite-plugin-fonts';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), eslintPlugin({
-    include: 'src/**/*.+(js|jsx)',
-    fix: true
-  }),]
-})
+    plugins: [
+        reactRefresh(),
+        eslintPlugin({
+            include: 'src/**/*.+(js|jsx)',
+            fix: true,
+        }),
+        ViteFonts({
+            google: {
+                families: [
+                    {
+                        name: 'Karla',
+                        styles: 'wght@400;700',
+                    },
+                    {
+                        name: 'Lato',
+                        styles: 'wght@400;700;900',
+                    },
+                ],
+            },
+        }),
+    ],
+});
