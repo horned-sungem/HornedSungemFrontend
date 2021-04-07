9d341eae077ab36665d0215be2d77b74a2995949
Inluminous <lenmisch@gmail.com>
2021-04-07 20:19:17 +0200

    Add UserContext

--- src/common/App.jsx
+++ src/common/App.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import {
     Switch,
     BrowserRouter as Router,
 } from 'react-router-dom';
+import { UserContext } from './UserContext';
 
 export default function App() {
-  return (
-    <div className="App">
-        <h1>Test</h1>
-        <Router>
-            <Switch>
-                {/*<Route component={App} path='/' exact/> */}
-            </Switch>
-        </Router>  
-    </div>
-  )
+
+    const [user, setUser] = useState(null);
+    const contextValue = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+    return (
+        <div className="App">
+            <h1>Test</h1>
+            <UserContext.Provider value={contextValue}>
+                <Router>
+                    <Switch>
+                        {/*<Route component={App} path='/' exact/> */}
+                    </Switch>
+                </Router>  
+            </UserContext.Provider>
+        </div>
+    )
 }
