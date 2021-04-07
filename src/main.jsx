747744e797962e7b242d0cfcb3b76736528e229c
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2021-04-07 22:53:21 +0200

    added basic styling and removed old font loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './common/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './common/App';
+import './styles/main.sass';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
