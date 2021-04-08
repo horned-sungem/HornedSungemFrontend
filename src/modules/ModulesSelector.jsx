cdf8c89f81d9d60f043080308c4b6e620a778a8c
Inluminous <lenmisch@gmail.com>
2021-04-09 01:26:13 +0200

    restructure & decouple modules

---
+++ src/modules/ModulesSelector.jsx
@@ -0,0 +1,11 @@
+import { Box, TextInput } from 'grommet';
+import React from 'react';
+
+export const ModulesSelector = (props) => {
+    return (
+        <Box>
+            CP <TextInput />
+            Name <TextInput />
+        </Box>
+    )
+}
