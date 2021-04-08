8f74a8752860c412f23bc3f28c4e289dae83e507
Inluminous <lenmisch@gmail.com>
2021-04-09 00:53:15 +0200

    Add NavLinks to routing

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button, Nav, Sidebar } from "grommet";
 import { BladesHorizontal, Chat, Home } from 'grommet-icons';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => (
     <Sidebar background='brand'>
         <Nav gap='small'>
-            <Button icon={<Home />} />
-            <Button icon={<BladesHorizontal />} />
-            <Button icon={<Chat />} />
+            <Button as={NavLink} to='/' icon={<Home />}/> 
+            <Button as={NavLink} to='/modules/' icon={<BladesHorizontal />}/> 
+            <Button as={NavLink} to='/modules/' icon={<Chat />}/> 
         </Nav>
     </Sidebar>
 )
