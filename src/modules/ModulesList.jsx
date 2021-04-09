a533ed7e959593366eeff4e9394282367dea5457
Inluminous <lenmisch@gmail.com>
2021-04-09 02:14:27 +0200

    Add link to module details page

--- src/modules/ModulesList.jsx
+++ src/modules/ModulesList.jsx
@@ -1,15 +1,21 @@
 import { Box, List } from 'grommet';
 import React from 'react';
+import { useHistory } from 'react-router';
 
-export const ModulesList = (props) => ( 
-    <Box width='large'>
-        <List
-            primaryKey='name'
-            secondaryKey='cp'
-            data={props.modules}
-            paginate
-            step={15}>
-                {module => <div onMouseOver={() => props.setChosenModule(module)}>{module.name} with {module.cp} CP</div>}    
-        </List>
-    </Box>
-)
+export const ModulesList = (props) => {
+
+    let history = useHistory();
+
+    return ( 
+        <Box width='large'>
+            <List
+                primaryKey='name'
+                secondaryKey='cp'
+                data={props.modules}
+                paginate
+                step={15}>
+                    {module => <div onMouseOver={() => props.setChosenModule(module)} onClick={() => history.push('/module/' + module.id)}>{module.name} with {module.cp} CP</div>}    
+            </List>
+        </Box>
+    );
+};
