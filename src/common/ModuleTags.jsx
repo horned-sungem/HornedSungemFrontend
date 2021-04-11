792966886ceeeaed0098c5b761f9dd1d266a3c9b
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2021-04-11 14:56:44 +0200

    applied tag design

--- src/common/ModuleTags.jsx
+++ src/common/ModuleTags.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 import { CPTag, SemesterTag } from './Tag';
 
 export const ModuleTags = ({ module }) => (
-    <Box direction='row' justify='center' gap='small' height={{min: 'auto'}} wrap={true}>
+    <Box
+        direction='row'
+        justify='center'
+        gap='small'
+        height={{ min: 'auto' }}
+        wrap={true}>
         <SemesterTag>{module.cycle}</SemesterTag>
         <CPTag>{module.cp}</CPTag>
     </Box>
-)
+);
--- src/common/Tag.jsx
+++ src/common/Tag.jsx
@@ -1,32 +1,21 @@
-import { Box } from 'grommet';
+import { Box, Text } from 'grommet';
 import React from 'react';
 
-export const Tag = ({children, color}) => (
-    <Box background={color} round pad='xsmall' alignSelf='start' style={{ whiteSpace: 'nowrap'}} elevation='small'>
-        {children}
+export const Tag = ({ children, label }) => (
+    <Box round align='center' direction='column' pad='xsmall' alignSelf='start'>
+        <Text weight='bold' color='brand' size='large'>
+            {children}
+        </Text>
+        <Text color='brand' size='small'>
+            {label}
+        </Text>
     </Box>
-)
+);
 
 export const CPTag = ({ children }) => {
-    const chroma = Math.max(0, Math.min(120, parseInt(children) * 12))
-    const color = 'hsl(' + chroma + ', 100%, 65%)'
-    return (
-       <Tag color={color}>{children} CP</Tag>
-    )
-}
+    return <Tag label='CP'>{children}</Tag>;
+};
 
 export const SemesterTag = ({ children }) => {
-    let color = 'neutral-2'
-    if (children.includes('Wintersemester')) {
-        color = 'accent-3'
-    }
-    if (children.includes('Sommersemester')) {
-        color = 'neutral-1'
-        if (children.includes('Wintersemester')) {
-            color = 'neutral-3'
-        }
-    }
-    return (
-        <Tag color={color}>{children}</Tag>
-    )
-}
+    return <Tag label='Semester'>{children}</Tag>;
+};
