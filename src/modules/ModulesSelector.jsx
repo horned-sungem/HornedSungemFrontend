97915cdfd977a8bbe8b310c3dc2d6f7220531f72
Inluminous <lenmisch@gmail.com>
2021-04-11 22:31:31 +0200

    Add string search

--- src/modules/ModulesSelector.jsx
+++ src/modules/ModulesSelector.jsx
@@ -6,25 +6,28 @@ export const ModulesSelector = ({ setModuleFilter }) => {
     const [minCp, setMinCp] = useState(null);
     const [maxCp, setMaxCp] = useState(null);
 
+    const [searchString, setSearchString] = useState('');
+
     useEffect(() => {
 
         const filters = [
             module => (!minCp) || module.cp >= parseInt(minCp),
-            module => (!maxCp) || module.cp <= parseInt(maxCp)
+            module => (!maxCp) || module.cp <= parseInt(maxCp),
+            module => module.name.includes(searchString)
         ]
 
         const moduleFilter = (module) => !(filters.map((filt) => filt(module)).includes(false))
 
         setModuleFilter(() => moduleFilter)
 
-    }, [minCp, maxCp])
+    }, [minCp, maxCp, searchString])
 
     return (
         <Box height={{min: 'auto'}}>
             <Box direction='row' align='center' gap='small'>
                 CP from <TextInput onChange={event => setMinCp(event.target.value)} /> to <TextInput onChange={event => setMaxCp(event.target.value)} />
             </Box>
-            Name <TextInput />
+            Name <TextInput onChange={event => setSearchString(event.target.value)}/>
         </Box>
     )
 }
