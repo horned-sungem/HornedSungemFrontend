9bc5c4f31605cdb246de6f4cb4db0986ea942c06
joynere <nicokunznk00@gmail.com>
2021-04-13 19:49:52 +0200

    Add Scrollbar to ModulesDescription on overflow

--- src/modules/ModulesDescription.jsx
+++ src/modules/ModulesDescription.jsx
@@ -4,7 +4,7 @@ import { DetailsInformation } from '../details/DetailsInformation';
 
 export const ModulesDescription = (props) => {
     return (
-        <Box flex>
+        <Box flex overflow='auto'>
             <Header background='brand' pad='small'>
                 <h3 style={{ margin: '1rem 0' }}>
                     {props.chosenModule.name} - {props.chosenModule.id}
