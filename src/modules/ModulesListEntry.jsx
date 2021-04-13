128e918ba7f1217d56e233204f5debf30e642dfa
joynere <nicokunznk00@gmail.com>
2021-04-14 00:52:49 +0200

    Make ModuleListEntries rounder

--- src/modules/ModulesListEntry.jsx
+++ src/modules/ModulesListEntry.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const ModulesListEntry = ({ children, module }) => (
     <Box
         pad='small'
-        round='small'
+        round='medium'
         background='#F2F2F2'
         direction='row'
         gap='none'
