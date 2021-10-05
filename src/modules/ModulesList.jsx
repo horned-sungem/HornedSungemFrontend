a829245857a3c40d7a3b78dbf2d5596c649fec8a
Julian Imhof <julianimhof00@gmail.com>
2021-10-05 02:15:10 +0200

    changed InfiniteScroll to Virtuoso cirtual scroller
    fixes #18

--- src/modules/ModulesList.jsx
+++ src/modules/ModulesList.jsx
@@ -1,10 +1,11 @@
 import React, { useContext  } from 'react';
-import { Box, InfiniteScroll } from 'grommet';
+import { Box } from 'grommet';
 import { useHistory } from 'react-router';
 import { ModulesListEntry } from './ModulesListEntry';
 import { ModuleTags } from '../common/ModuleTags';
 import { VotesContext } from '../common/VotesContext';
 import './styles/Modules.sass'
+import { Virtuoso } from 'react-virtuoso';
 
 export const ModulesList = (props) => {
 
@@ -12,31 +13,36 @@ export const ModulesList = (props) => {
 
     const { votes } = useContext(VotesContext);
 
-    const vote_ids = votes.map(vote => vote[0].nr)
+    const vote_ids = votes.map(vote => vote[0].nr);
+
+    function module (index) {
+        var module = Object.values(props.modules)[index];
+        return (
+            <Box
+                flex={false}
+                onMouseOver={() => props.setChosenModule(module)}
+                height={{min: 'xsmall'}}
+                className={'list__item' + (vote_ids.includes(module.nr) ? ' list__item--voted' : '')}
+                justify='center'
+                border='bottom'
+                onClick={() =>
+                    history.push(
+                        '/module/' + module.nr.replace('/', '_')
+                    )
+                }>
+                <ModulesListEntry module={module}>
+                    <ModuleTags module={module} />
+                </ModulesListEntry>
+            </Box>
+        )
+    }
+
 
     return (
-        <Box width='full' overflow='auto'>
-            <InfiniteScroll items={Object.values(props.modules)} step={10}>
-                {(module, idx) => (
-                    <Box
-                        key={idx}
-                        flex={false}
-                        onMouseOver={() => props.setChosenModule(module)}
-                        height={{min: 'xsmall'}}
-                        className={'list__item' + (vote_ids.includes(module.nr) ? ' list__item--voted' : '')}
-                        justify='center'
-                        border='bottom'
-                        onClick={() =>
-                            history.push(
-                                '/module/' + module.nr.replace('/', '_')
-                            )
-                        }>
-                        <ModulesListEntry module={module}>
-                            <ModuleTags module={module} />
-                        </ModulesListEntry>
-                    </Box>
-                )}
-            </InfiniteScroll>
-        </Box>
+        <Virtuoso 
+            height={'full'}
+            totalCount={props.modules.length}
+            itemContent={module}
+        />
     );
 };
